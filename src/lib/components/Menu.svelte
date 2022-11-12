bb75297dc2c6b0032299be62f3b527a9c3a15b06
Alexandru Bracau <alexandrubracau@gmail.com>
2022-11-12 22:19:23 +0200

    Refactor Menu

--- src/lib/components/Menu.svelte
+++
@@ -1,6 +0,0 @@
-<div class="dropdown">
-    <label tabindex="0" class="btn btn-outline btn-square normal-case w-36 text-3xl">raucity!</label>
-    <ul tabindex="0" class="dropdown-content menu shadow bg-base-100 w-36 mt-2">
-      <slot />
-    </ul>
-  </div>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,12 +1,14 @@
 <script>
     import Identity from "./Identity.svelte";
-    import Menu from "./Menu.svelte";
 </script>
 <div class="navbar">
     <div class="flex-1">
-        <Menu> 
-            <slot />
-        </Menu>
+        <div class="dropdown">
+            <label tabindex="0" class="btn btn-outline btn-square normal-case w-36 text-3xl">raucity!</label>
+            <ul tabindex="0" class="dropdown-content menu shadow bg-base-100 w-36 mt-2">
+                <slot />
+            </ul>
+        </div>
     </div>
     <div class="flex-none">
         <Identity></Identity>
