aba7336d9033f6206ff874ba09c1488ab3cef3e2
Alexandru Bracau <alexandrubracau@gmail.com>
2022-11-13 18:37:22 +0200

    Data framework, hooked to local storage

---
+++ src/lib/ui/Test.svelte
@@ -0,0 +1,13 @@
+<button class="btn btn-outline" on:click={writeUser}> Write </button>
+<button class="btn btn-outline" on:click={readUser}> Read </button>
+
+<script>
+    import { Data } from "$lib/db/Data";
+    import { User } from "$lib/db/DataStructure";
+
+    let testUser = new User();
+
+    let writeUser = () => { Data.write(testUser) }
+    let readUser = () => { Data.read(User.name, testUser.key) }
+
+</script>
