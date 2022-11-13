d058a17bf19f0f8c2805843f55a5d6c7d70366c2
Alexandru Bracau <alexandrubracau@gmail.com>
2022-11-13 14:15:40 +0200

    Logout button, refactoring

--- src/lib/auth/Identity.svelte
+++ src/lib/auth/Identity.svelte
@@ -2,24 +2,6 @@
     <script src="https://accounts.google.com/gsi/client" async defer></script>
 </svelte:head>
 
-<script lang="ts">
-    import { onMount } from 'svelte';
-    import { idConfig } from './Identity';
-
-    let signInPlaceholder: HTMLDivElement;
-
-    onMount(async () => {
-        // Initialise Google Identity Context
-        google.accounts.id.initialize(idConfig);
-        // Render sign in button
-        google.accounts.id.renderButton(
-            signInPlaceholder,
-            { type: "standard", theme: "outline", text: "signin", size: "medium", logo_alignment: "left" } 
-          );
-    });
-</script>
-
-
 <div class="dropdown dropdown-end">
     <label tabindex="0" id="identity" class="btn placeholder">
         <div class="bg-neutral-focus text-neutral-content">
@@ -28,5 +10,22 @@
     </label>
     <ul tabindex="0" class="dropdown-content menu shadow bg-base-100 mt-2">
         <li><div bind:this={signInPlaceholder}></div></li>
+        <li><a class="g_id_signout" on:click={Identity.logOut}>Logout</a></li>
     </ul>
 </div>
+
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { Identity } from './Identity';
+
+    let signInPlaceholder: HTMLElement
+
+    onMount(async () => {
+        // Initialise Google Identity Context
+        Identity.initialiseContext(google.accounts);
+        // Render sign in button
+        google.accounts.id.renderButton(
+            signInPlaceholder,
+            { type: "standard", theme: "outline", text: "signin", size: "medium", logo_alignment: "left" });
+    });
+</script>
--- src/lib/auth/Identity.ts
+++ src/lib/auth/Identity.ts
@@ -1,26 +1,84 @@
-import type { IdConfiguration, CredentialResponse } from "google-one-tap";
+import type { IdConfiguration, CredentialResponse, RevocationResponse, accounts } from "google-one-tap";
 import jwt_decode from "jwt-decode";
 import { googleClientId } from "$lib/config";
 
-
-let handleCredentialResponse = (response: CredentialResponse) => { 
-    console.log('Response: ', response);
-    let jwt = jwt_decode(response.credential);
-    console.log('JWT: ', jwt);
-}
-export var idConfig: IdConfiguration = {
-    client_id: googleClientId,
-    context: 'use',
-    ux_mode: 'popup',
-    callback: handleCredentialResponse
+export interface GoogleJWTPayload {
+    iss: string; // e.g.:"https://accounts.google.com", The JWT's issuer
+    nbf: number; // e.g.:161803398874
+    aud: string; // e.g.:"314159265-pi.apps.googleusercontent.com", Your server's client ID
+    sub: string; // e.g.:"3141592653589793238", The unique ID of the user's Google Account
+    hd: string; // e.g.:"gmail.com", If present, the host domain of the user's GSuite email address
+    email: string; // e.g.:"elisa.g.beckett@gmail.com", The user's email address
+    email_verified: boolean; // e.g.: true, If Google has verified the email address
+    azp: string; // e.g.: "314159265-pi.apps.googleusercontent.com"
+    name: string; // e.g.: "Elisa Beckett"
+    picture: string; // e.g.: If present, a URL to user's profile picture
+    given_name: string; // e.g.: "Elisa"
+    family_name: string; // e.g.: "Beckett"
+    iat: number; // e.g.:1596474000, Unix timestamp of the assertion's creation time
+    exp: number; // e.g.:1596477600, Unix timestamp of the assertion's expiration time
+    jti: string; // e.g.:"abc161803398874def"
 }
 
 export class Identity {
-    #jwt: any;
-    
-    constructor(response: CredentialResponse) {
-        console.log('Response: ', response);
-        this.#jwt = jwt_decode(response.credential);
-        console.log('JWT: ', this.#jwt);
+    static #instance: Identity;
+
+    jwt?: GoogleJWTPayload;
+    api: accounts;
+    loggedIn: boolean;
+
+    constructor(api: accounts) {
+        this.api = api;
+        this.loggedIn = false;
+    }
+
+    public static logIn (response: CredentialResponse) {
+        let instance = Identity.#instance;
+        console.log('Got credential from ', response.select_by);
+        try {
+            instance.jwt = jwt_decode(response.credential);
+            console.log('Decoded JWT: ', instance.jwt);
+        }
+        catch(e) {
+            console.error(e);
+        }
+        finally {
+            instance.loggedIn = true;
+            console.log('Logged in', instance.loggedIn);
+        }
+    }
+
+    public static logOut () {
+        let instance = Identity.#instance;
+        console.log("Logging out...");
+
+        function onLogOut(response: RevocationResponse) {
+            console.log('Logout success: ', response.successful);
+        }
+        
+        console.log('Is logged in? ', instance.loggedIn);
+        if(instance.loggedIn && instance.jwt) {
+            instance.api.id.revoke(instance.jwt.sub, onLogOut);
+            instance.loggedIn = false;
+        }
+    }
+
+    public static initialiseContext (api: accounts) {
+        // Construct instance
+        let instance = new Identity(api);
+
+        // Google Identity settings
+        let idConfig: IdConfiguration = {
+            client_id: googleClientId,
+            // auto_select: true,
+            context: 'use',
+            ux_mode: 'popup',
+            callback: Identity.logIn
+        }
+
+        // Call the Identity API
+        instance.api.id.initialize(idConfig);
+
+        Identity.#instance = instance;
     }
 }
