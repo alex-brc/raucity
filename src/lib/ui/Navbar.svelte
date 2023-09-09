49b628ae754b34077876af56014d15d32b6e9725
Alexandru Bracau <alexandrubracau@gmail.com>
2023-09-09 22:45:36 +0300

    Fixed #26

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -2,7 +2,7 @@
     import Identity from "$lib/id/Identity.svelte"
 </script>
 
-<div class="navbar">
+<div class="navbar" style="align-items: flex-start;">
     <div class="flex-1">
         <button class="btn btn-outline btn-square normal-case w-36 text-3xl">raucity!</button>
     </div>
